c8a34b851d6ff9d7cd76de0be456fa41e711777d
gabrielmoreira02 <moreiragabriel.aires@gmail.com>
2018-09-04 13:18:29 -0300

    Add files via upload

--- funcs.py
+++ funcs.py
@@ -1,5 +1,43 @@
 import numpy
 
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    # BUILDMAXHEAP
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+
+    #Ordenacao
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+
+
+
+
+
+
 def selectionSort(lista):
     for i in range(len(lista)):
         minPosition = i
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def marcadordetempo(function):
 
 while True:
     tipo = int(input("Qual funcao voce deseja testar? (1 para selectionSort, 2 para insertionSort, 3 para mergeSort, 4 para quickSort,"
-                     "5 para countingSort e 6 para radix):     "))
+                     "5 para countingSort, 6 para radix e 7 para heap):     "))
     qtd = int(input("Com quantos elementos voce deseja testar?(100, 1000, 10000 ou 100000):            "))
 
     if qtd == 100:
@@ -48,6 +48,8 @@ while True:
         marcadordetempo(countingSort)
     elif tipo == 6:
         marcadordetempo(radix_sort)
+    elif tipo == 7:
+        marcadordetempo(heapSort)
     else:
         print("opcao invalida")
         continue
